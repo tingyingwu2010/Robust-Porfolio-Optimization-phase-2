85ecdcba57b48a63c17d29f96d0ec697271ff99c
girach <mdbilalgirach7@gmail.com>
2018-10-05 20:39:08 +0530

    Added the code for Joint uncertainty and SR ratios for low and high risk.

--- code.m
+++ code.m
@@ -2,7 +2,7 @@
 clc;
 clear all;
 
-table = readtable('final_list.csv');
+table = readtable('./data_related/final_list100.csv');
 
 stock_prices = table{:,2:end};
 
@@ -16,24 +16,24 @@ lambda = 1/100;
 maxim = @(x) (lambda*x'*covariance*x - mu'*x);
 
 
-init = rand(31,1);
+init = rand(size(stock_prices,2),1);
 init = init./sum(init);
 options = optimoptions(@fmincon,'Algorithm','sqp','MaxIterations',4000);
-options.MaxFunctionEvaluations = 10000;
-[x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,31),1,[],[],[],options);
+options.MaxFunctionEvaluations = 20000;
+[x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,size(stock_prices,2)),1,[],[],[],options);
 
-risk_aversion = 1000:100:20000;
+risk_aversion = 100:100:20000;
 mean_vals_mark = [];
 sd_vals_mark = [];
 for i=1:size(risk_aversion,2)
     
     lambda = risk_aversion(1,i)
-    init = rand(31,1);
+    init = rand(size(stock_prices,2),1);
     init = init./sum(init);
     maxim = @(x) (lambda*x'*covariance*x - mu'*x);
     options = optimoptions(@fmincon,'Algorithm','sqp','MaxIterations',4000);
-    options.MaxFunctionEvaluations = 10000;
-    [x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,31),1,[],[],[],options);
+    options.MaxFunctionEvaluations = 20000;
+    [x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,size(stock_prices,2)),1,[],[],[],options);
     
     
     mean_vals_mark = [mean_vals_mark, mu'*x];
@@ -55,13 +55,13 @@ alpha = 0.05;
 for i=1:size(risk_aversion,2)
     
     lambda = risk_aversion(1,i)
-    init = rand(31,1);
+    init = rand(size(stock_prices,2),1);
     init = init./sum(init);
     delta = abs(norminv(alpha/2)).*sqrt(diag(covariance))./size(stock_prices,1);
     maxim = @(x) (lambda*x'*covariance*x - mu'*x + delta'*abs(x));
     options = optimoptions(@fmincon,'Algorithm','sqp','MaxIterations',4000);
-    options.MaxFunctionEvaluations = 10000;
-    [x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,31),1,[],[],[],options);
+    options.MaxFunctionEvaluations = 20000;
+    [x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,size(stock_prices,2)),1,[],[],[],options);
     
     
     mean_vals_box = [mean_vals_box, mu'*x];
@@ -82,17 +82,16 @@ end
 mean_vals_ellipsoid = [];
 sd_vals_ellipsoid = [];
 
-alpha = 0.05;
 for i=1:size(risk_aversion,2)
     
     lambda = risk_aversion(1,i)
-    init = rand(31,1);
+    init = rand(size(stock_prices,2),1);
     init = init./sum(init);
     delta = chi2inv(alpha,size(stock_prices,2));
-    maxim = @(x) (lambda*x'*covariance*x - mu'*x + delta*sqrt(x'*covariance*x));
+    maxim = @(x) (lambda*x'*covariance*x - mu'*x + delta*sqrt(x'*covariance*x./(size(stock_prices,1))));
     options = optimoptions(@fmincon,'Algorithm','sqp','MaxIterations',4000);
-    options.MaxFunctionEvaluations = 10000;
-    [x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,31),1,[],[],[],options);
+    options.MaxFunctionEvaluations = 20000;
+    [x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,size(stock_prices,2)),1,[],[],[],options);
     
     
     mean_vals_ellipsoid = [mean_vals_ellipsoid, mu'*x];
@@ -101,11 +100,75 @@ for i=1:size(risk_aversion,2)
 end
 
 
+lower_mu = zeros(size(stock_prices,2),1);
+upper_mu = zeros(size(stock_prices,2),1);
+lower_cov = zeros(size(stock_prices,2),size(stock_prices,2));
+upper_cov = zeros(size(stock_prices,2),size(stock_prices,2));
+
+iterations = 8000;
+mu_matrix = zeros(size(stock_prices,2),iterations);
+cov_matrix = zeros(size(stock_prices,2),size(stock_prices,2),iterations);
+rows = 1:size(stock_prices,1);
+
+for i=1:iterations
+    
+   temp_rows = datasample(rows,size(stock_prices,1));
+   temp_data = stock_prices(temp_rows,:);
+   mu_matrix(:,i) = mean(temp_data)';
+   cov_matrix(:,:,i) = cov(temp_data);
+
+%     temp_data = mvnrnd(mu,covariance,size(stock_prices,1));
+%     mu_matrix(:,i) = mean(temp_data)';
+%     cov_matrix(:,:,i) = cov(temp_data);
+    
+end
+
+for i = 1:size(stock_prices,2)
+
+    temp = sort(mu_matrix(i,:));
+    lower_mu(i,1) = temp(iterations*0.025);
+    upper_mu(i,1) = temp(iterations*0.975);
+    
+end
+
+
+for i = 1:size(stock_prices,2)
+
+    for j=1:size(stock_prices,2)
+        
+       temp = sort(cov_matrix(i,j,:));
+       lower_cov(i,j) = temp(iterations*0.025);
+       upper_cov(i,j) = temp(iterations*0.975);
+      
+    end
+    
+end
+
+mean_vals_var = [];
+sd_vals_var = [];
+
+% Joint UNcertainty
+for i=1:size(risk_aversion,2)
+    
+    lambda = risk_aversion(1,i)
+    init = rand(size(stock_prices,2),1);
+    maxim = @(x) (lambda*x'*upper_cov*x - lower_mu'*x);
+    options = optimoptions(@fmincon,'Algorithm','sqp','MaxIterations',4000);
+    options.MaxFunctionEvaluations = 20000;
+    [x,fval,exitflag,output] = fmincon(maxim,init,[],[],ones(1,size(stock_prices,2)),1,[],[],[],options);
+    
+    
+    mean_vals_var = [mean_vals_var, mu'*x];
+    sd_vals_var = [sd_vals_var, sqrt(x'*covariance*x)];
+    
+end
+
 figure(1), hold on
 plot(sd_vals_mark, mean_vals_mark);
 plot(sd_vals_box, mean_vals_box);
 plot(sd_vals_ellipsoid, mean_vals_ellipsoid);
-legend('Markowitz','With box uncertainty','With ellipsoid uncertainty');
+plot(sd_vals_var, mean_vals_var);
+legend('Markowitz','With box uncertainty','With ellipsoid uncertainty','Joint Variance');
 ylabel('return');
 xlabel('Standard Deviation')
 hold off
@@ -114,9 +177,18 @@ risk_free = log(1.06)/365;
 mark = (mean_vals_mark - risk_free)./sd_vals_mark;
 box = (mean_vals_box - risk_free)./sd_vals_box;
 ellipsoid = (mean_vals_ellipsoid - risk_free)./sd_vals_ellipsoid;
+joint_var = (mean_vals_var - risk_free)./sd_vals_var;
 
+figure(2), hold on
+plot(risk_aversion, mark);
+plot(risk_aversion, box);
+plot(risk_aversion, ellipsoid);
+plot(risk_aversion, joint_var);
+legend('Markowitz','With box uncertainty','With ellipsoid uncertainty', 'Joint Variance');
+ylabel('Sharpe Ratio');
+xlabel('Risk Aversion')
 
-
+hold off
 
 % figure(2)
 % plot(1000:100:15000,mean_vals);
@@ -140,9 +212,9 @@ ellipsoid = (mean_vals_ellipsoid - risk_free)./sd_vals_ellipsoid;
 %    mu_matrix(:,i) = mean(temp_data)';
 %    cov_matrix(:,:,i) = cov(temp_data);
 
-    temp_data = mvnrnd(mu,covariance,size(stock_prices,1));
-    mu_matrix(:,i) = mean(temp_data)';
-    cov_matrix(:,:,i) = cov(temp_data);
+%     temp_data = mvnrnd(mu,covariance,size(stock_prices,1));
+%     mu_matrix(:,i) = mean(temp_data)';
+%     cov_matrix(:,:,i) = cov(temp_data);
     
 % end
 % 
