d079658b803ddb43c07c5ca8d21453dfcf94bf73
shera2018 <shashank444oberoi@gmail.com>
2020-02-18 23:04:46 +0530

    Populating Data for SnP

--- data_related/download.py
+++ data_related/download.py
@@ -1,30 +1,44 @@
-import matplotlib.pyplot as plt
-import fix_yahoo_finance as yf
+# import matplotlib.pyplot as plt
+# import fix_yahoo_finance as yf
+import yfinance as yf
 import pandas as pd
-d1=pd.read_csv("SnP100.csv") 
+# d1=pd.read_csv("SnP100.csv")
+d1=pd.read_csv("SnP500.csv")
 comp_names=d1["Security Name"].tolist()
  
 f='2016-12-18'
+# f='2017-12-18'
 t='2018-09-30'
 new_array = [];
 total_data=list()
 for i in range(len(comp_names)):
-	comp_symbol=comp_names[i].strip()+'.BO'
-	if (comp_symbol == 'IDEA.BO' or comp_symbol == 'CONCOR.BO' or comp_symbol == 'HDFCLIFE.BO'):
-		continue
-	print comp_symbol
-	data = yf.download(comp_symbol,f,t)
-	adj_close=data["Adj Close"].tolist()
-	total_data.append(adj_close)
-	print len(adj_close)
-	new_array.append(comp_symbol)
-	
+    # comp_symbol=comp_names[i].strip()+'.BO'
+    comp_symbol=comp_names[i].strip()
+    # if (comp_symbol == 'IDEA.BO' or comp_symbol == 'CONCOR.BO' or comp_symbol == 'HDFCLIFE.BO'):
+        # continue
+    print(comp_symbol)
+    try:
+        data = yf.download(comp_symbol,f,t)
+    except:
+        print("Download failed for ticker symbol: "+comp_symbol)
+        continue
+    # data = yf.download(comp_symbol,f,t)
+    if (len(data)==0):
+        print("Download failed for ticker symbol: "+comp_symbol)
+        continue
+    print(len(data))
+    adj_close=data["Adj Close"].tolist()
+    total_data.append(adj_close)
+    print(len(adj_close))
+    new_array.append(comp_symbol)
+    
 df=pd.DataFrame(total_data)
 
 df = df.transpose()
 df.columns = new_array
-print df
-df.to_csv('final_snp100.csv')
+print(df)
+# df.to_csv('final_snp100.csv')
+df.to_csv('final_snp500.csv')
 #print df
 #data = yf.download('INFY.BO',f,t)
 #print type(data)
