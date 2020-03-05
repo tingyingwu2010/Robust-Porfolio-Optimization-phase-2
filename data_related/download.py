1b198c84607aca2383f21422ffeeff410dca559c
shera2018 <shashank444oberoi@gmail.com>
2020-03-05 20:10:21 +0530

    Populating data for Nikkei and FTSE indices

--- data_related/download.py
+++ data_related/download.py
@@ -5,7 +5,12 @@ import pandas as pd
 import math
 import numpy as np
 # d1=pd.read_csv("SnP100.csv")
-d1=pd.read_csv("SnP500.csv")
+# d1=pd.read_csv("SnP500.csv")
+# d1=pd.read_csv("Nikkei225.csv")
+# d1=pd.read_csv("JPXNikkei400.csv")
+# d1=pd.read_csv("ftse100.csv")
+d1=pd.read_csv("ftse350.csv")
+
 comp_names=d1["Security Name"].tolist()
  
 f='2016-12-18'
@@ -15,7 +20,10 @@ new_array = [];
 total_data=list()
 for i in range(len(comp_names)):
     # comp_symbol=comp_names[i].strip()+'.BO'
-    comp_symbol=comp_names[i].strip()
+    # comp_symbol=comp_names[i].strip()
+    comp_symbol=comp_names[i].strip()+'.L'
+    # comp_symbol=comp_names[i].strip()+'.T'
+    # comp_symbol=str(comp_names[i]).strip()+'.T'
     # if (comp_symbol == 'IDEA.BO' or comp_symbol == 'CONCOR.BO' or comp_symbol == 'HDFCLIFE.BO'):
     #     continue
     print(comp_symbol)
@@ -45,7 +53,11 @@ df = df.transpose()
 df.columns = new_array
 print(df)
 # df.to_csv('final_snp100.csv')
-df.to_csv('final_snp500.csv')
+# df.to_csv('final_snp500.csv')
+# df.to_csv('final_nikkei225.csv')
+df.to_csv('final_ftse350.csv')
+
+
 #print df
 #data = yf.download('INFY.BO',f,t)
 #print type(data)
