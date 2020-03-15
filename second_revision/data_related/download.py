af223cb78ea349c644601af76379419a7fc68b8a
shera2018 <shashank444oberoi@gmail.com>
2020-03-15 23:59:52 +0530

    Populating FTSE data (removing negative stock values)

--- second_revision/data_related/download.py
+++ second_revision/data_related/download.py
@@ -26,7 +26,9 @@ for i in range(len(comp_names)):
     # comp_symbol=str(comp_names[i]).strip()+'.T'
     # if (comp_symbol == 'IDEA.BO' or comp_symbol == 'CONCOR.BO' or comp_symbol == 'HDFCLIFE.BO'):
     #     continue
-    print(comp_symbol)
+    if (comp_symbol == 'ATST.L'):
+        continue
+    # print(comp_symbol)
     try:
         data = yf.download(comp_symbol,f,t)
     except:
@@ -38,14 +40,14 @@ for i in range(len(comp_names)):
         continue
     print(len(data))
     adj_close=data["Adj Close"].tolist()
-    
+    print(adj_close)
     if True in np.isnan(adj_close):
         print("NaN values for ticker symbol: "+comp_symbol)
         continue
     # print(adj_close)
-    is_price_negative=any(n < 0 for n in adj_close)
+    is_price_negative=any(n <= 0 for n in adj_close)
     if (is_price_negative):
-        print("Negative Stock values for ticker symbol: "+comp_symbol)
+        print("Negative or Zero Stock values for ticker symbol: "+comp_symbol)
         continue
 
    
@@ -56,7 +58,9 @@ for i in range(len(comp_names)):
 df=pd.DataFrame(total_data)
 
 df = df.transpose()
+
 df.columns = new_array
+df=df.dropna(1)
 print(df)
 # df.to_csv('final_snp100.csv')
 # df.to_csv('final_snp500.csv')
